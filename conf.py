590c5c537bfa545df3cf44e73ed590223689d5ef
Chris Kruger <chris.kruger@krugerheavyindustries.com>
2016-10-21 14:47:37 +0800

    removed extension

--- conf.py
+++ conf.py
@@ -30,7 +30,6 @@ import sys, os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxcontrib.dotnetdomain'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
